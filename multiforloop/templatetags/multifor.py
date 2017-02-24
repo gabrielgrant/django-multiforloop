a0f4c22bededf84ad0cd9915751bd3e5679d9103
Tomer Chachamu <tomer.chachamu@gmail.com>
2017-02-24 20:52:40 +0000

    fix import

--- multiforloop/templatetags/multifor.py
+++ multiforloop/templatetags/multifor.py
@@ -1,6 +1,5 @@
 from itertools import izip_longest
 
-from django.template.base import Library
 from django.conf import settings
 
 register = Library()
@@ -13,7 +12,7 @@ for x, y in xy_list reversed
 for x in x_list reversed; y in y_list
 for x in x_list; y in y_list reversed
 """
-from django.template import Node, NodeList, Template, Context, Variable
+from django.template import Node, NodeList, Template, Context, Variable, Library
 from django.template import TemplateSyntaxError, VariableDoesNotExist
 
 

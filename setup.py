5ed828aa372d2b847395defa82cc9806078393df
Gabriel Grant <g@briel.ca>
2021-06-21 00:07:25 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-multiforloop',
-    version='0.2.0',
+    version='0.3.0',
     packages=['multiforloop', 'multiforloop.templatetags'],
     author='Gabriel Grant',
     author_email='g@briel.ca',

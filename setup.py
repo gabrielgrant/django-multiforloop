78adbe22be0810e9fa64f29df86aa4053bae32ae
Gabriel Grant <g@briel.ca>
2021-06-23 08:45:28 -0700

    Render README as markdown on PyPI and bump version again

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-multiforloop',
-    version='0.3.0',
+    version='0.3.1',
     packages=['multiforloop', 'multiforloop.templatetags'],
     author='Gabriel Grant',
     author_email='g@briel.ca',
     license='LGPL',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     url='http://github.org/gabrielgrant/django-multiforloop/',
 )
 
